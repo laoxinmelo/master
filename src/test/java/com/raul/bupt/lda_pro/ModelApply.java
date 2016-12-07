b1f86bc65a12d590266c0de7b857e1d00be0595e
laoxin.lx <laoxin.lx@alibaba-inc.com>
2016-12-07 17:27:44 +0800

    按照类别划分...

--- src/test/java/com/raul/bupt/lda_pro/ModelApply.java
+++ src/test/java/com/raul/bupt/lda_pro/ModelApply.java
@@ -24,6 +24,29 @@ public class ModelApply {
     //模型保存路径
     private static final String modelPath = ".\\lda\\model";
 
+    //抱歉类别
+    private static final int[] apologyIndex = {3,7};
+    //解释类别
+    private static final int[] explainIndex = {2,8,14};
+    //感激类别
+    private static final int[] thankIndex = {0,5,6,10};
+
+
+    /**
+     * 判断某个索引数组中是否包含该索引
+     * @param indexArray
+     * @param index
+     * @return
+     */
+    private static boolean indexContain(int[] indexArray,int index) {
+        for(int tempIndex:indexArray) {
+            if(tempIndex == index) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     /**
      * 读取所有已完成分词及去停的反馈数据
      * 并将其导入到模型当中，获取对应的主题向量
@@ -50,14 +73,50 @@ public class ModelApply {
                 }else {
                     reply = totalContent[1];
                 }
-                System.out.println(reply);
-                System.out.println(count + "__________________________________");
-                bw.write(reviewId + "\t" + RunInference.getDocTheta(inferencer, reply) + "\r\n");
+
+
+                double apologyValue = 0.0;
+                double explainValue = 0.0;
+                double thankValue = 0.0;
+
+                String[] tempVecArray = RunInference.getDocTheta(inferencer,reply).split(" ");
+                for(int i=0;i<tempVecArray.length;i++) {
+                    double tempValue = Double.valueOf(tempVecArray[i]);
+
+                    //抱歉类别对应权值
+                    if(indexContain(apologyIndex,i)) {
+                        if(tempValue>apologyValue) {
+                            apologyValue = tempValue;
+                        }
+                    }
+
+                    //解释类别对应权值
+                    if(indexContain(explainIndex,i)) {
+                        if(tempValue>explainValue) {
+                            explainValue = tempValue;
+                        }
+                    }
+
+                    //感激类别对应权值
+                    if(indexContain(thankIndex,i)) {
+                        if(tempValue>thankValue) {
+                            thankValue = tempValue;
+                        }
+                    }
+
+
+                }
+
+                double sum = apologyValue + explainValue + thankValue;
+
+                bw.write( reviewId + "\t" + apologyValue/sum + "\t" + explainValue/sum + "\t" + thankValue/sum  + "\r\n");
+                System.out.println( reviewId + "\t" + apologyValue/sum + "\t" + explainValue/sum + "\t" + thankValue/sum );
+                System.out.println("___________________________________");
                 count += 1;
             }
 
             bw.flush(); bw.close();
-        }catch(IOException e) {
+        }catch(Exception e) {
             e.printStackTrace();
         }
 
